f25ae71a8132ec8fa456e9ed1f4d3477ab138a47
John M. Nichol <jmnichol@gmail.com>
2021-03-27 08:08:19 -0400

    Update Decisions.py
    Add open file dialog, and fixed code typos

--- Decisions.py
+++ Decisions.py
@@ -2,7 +2,7 @@
 """
 Decisions.py 
 Analyzes the results of a slate query to find out when students make their
-decisions and how many admits are expected this year.
+decisions and how many admits are expected this year. This relies on using the most up to date date, so be sure to rerun the query before anaylzing the data.
 """
 
 import pandas
@@ -10,13 +10,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from datetime import datetime
+import tkinter as tk
+import tkinter.filedialog as fd
 
+root = tk.Tk()
+root.lift()
+root.attributes("-topmost", True)
+root.withdraw()
+filetypes=[("Excel files", ".xlsx .xls")]
+file_name= fd.askopenfilename(
+    parent=root,
+    title='Open a file',
+    initialdir='/',
+    filetypes=filetypes)
 
 plt.rc('font', family='sans-serif', size=16)
 
 #The data file should be an exported query with at least following fields:
 # Name,Decision 1 Confirmed Date,Decision 1,Institution 1 GPA (4.0 Scale),Period Year
-df= pandas.read_excel(r'C:\Users\Nichol\Downloads\Decision date 20210325-100623.xlsx')
+df= pandas.read_excel(file_name)
+
+#Alternatively, you can specify the file explicitly, as below.
+#df= pandas.read_excel(r'C:\Users\Nichol\Downloads\Decision date 20210325-100623.xlsx')
 
 #find the days before the due date the decision was made. The actual "deadline" is 4/16
 d={'year':df['Period Year'],'month':np.linspace(4,4,len(df['Period Year'])),
@@ -59,7 +74,6 @@ for year in years:
     if len(df_deferrals)==0:
         df_deferrals=pandas.DataFrame(d)
     
-    
     if year < current_year:
         accepts=np.linspace(1,1/len(df_accepts),len(df_accepts));
         declines=np.linspace(1,1/len(df_declines),len(df_declines));
@@ -72,7 +86,8 @@ for year in years:
     else:
         current_accepts=len(df_accepts)
         current_declines=len(df_declines)
-        
+
+#Fitting function        
 def func(x, a, b, c):
     return a * np.exp(-b * x) + c
 
@@ -82,6 +97,7 @@ ydata=total_accepts
 inds=np.argsort(xdata)
 xdata=xdata[inds]
 ydata=ydata[inds]
+#Lower and upper bounds for fitting. Below, the magnitude is constrained. 
 lb=[1,0,0]
 ub=[1+1e-5,100,1]
 popt_accept,pcov= curve_fit(func, xdata, ydata,bounds=(lb,ub))
@@ -94,10 +110,10 @@ plt.ylabel("Normalized cumulative accepts")
 plt.title("Accepts")
 plt.plot(xdata, func(xdata, *popt_accept), 'r-',
          label='fit: a=%5.3f, b=%5.3f, c=%5.3f' % tuple(popt_accept))
-
 plt.show()
 plt.legend()
 
+#Fitting function for the current year's data. Uses the fitted parameters above but this time fits the scaling.
 def func_accept(x, a):
     return a * np.exp(-popt_accept[1] * x) + popt_accept[2]
 
@@ -119,7 +135,7 @@ popt_accept_current,pcov=curve_fit(func_accept, xdata, ydata)
 # plt.legend()
 # =============================================================================
 
-print("Calculated accpets from fitting %3.3f and direct calculation %3.3f" %
+print("Calculated accepts from fitting: %3.3f and direct calculation: %3.3f" %
       (popt_accept_current[0]*current_accepts,
       current_accepts/func(days_to_deadline,*popt_accept)))
 
@@ -142,6 +158,7 @@ plt.plot(xdata, func(xdata, *popt_decline), 'r-',
 plt.show()
 plt.legend()
 
+#Same as above. Now only fit the intercept.
 def func_decline(x, a):
     return a * np.exp(-popt_decline[1] * x) + popt_decline[2]
 
@@ -164,12 +181,11 @@ popt_decline_current,pcov=curve_fit(func_decline, xdata, ydata)
 # plt.legend()
 # =============================================================================
 
-print("Calculated accpets from fitting %3.3f and direct calculation %3.3f" %
+print("Calculated declines from fitting: %3.3f and direct calculation: %3.3f" %
       (popt_decline_current[0]*current_declines,
       current_declines/func(days_to_deadline,*popt_decline)))
 
-#TODO: bin the mean times to acceptanceby GPA
-
+#Now let's analyze decision times according to GPA to find out how to use the wait list.
 gpa_bins=np.linspace(3,4,11)
 gpa_accepts_norm=np.empty([0]);
 gpa_accepts=np.empty([0]);
@@ -188,8 +204,8 @@ for gpa in gpa_bins[0:10]:
         
         gpa_accepts=np.append(gpa_accepts,len(df_accepts));
         gpa_accepts_norm=np.append(gpa_accepts_norm,len(df_accepts)/(len(df_accepts)+len(df_declines)));
-        gpa_days_accept=np.append(gpa_days_accept,df_accepts.days_before.mean());
-        gpa_days_decline=np.append(gpa_days_decline,df_declines.days_before.mean());
+        gpa_days_accept=np.append(gpa_days_accept,df_accepts.days_before.median());
+        gpa_days_decline=np.append(gpa_days_decline,df_declines.days_before.median());
 
 
 plt.figure(7,figsize=(10,10))    
@@ -199,7 +215,7 @@ plt.plot(gpa_bins[1:11],gpa_days_accept)
 plt.scatter(gpa_bins[1:11],gpa_days_decline,label='Declines')
 plt.plot(gpa_bins[1:11],gpa_days_decline)
 plt.xlabel("GPA")
-plt.ylabel("Days before 4/16 decision was made")
+plt.ylabel("Median days before 4/16 decision was made")
 plt.show()
 plt.legend()     
 
